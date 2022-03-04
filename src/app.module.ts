472d31b8719e7748a3cfb6e2af0e1ef5ecb83347
directsol <sergey.n.mamontov@gmail.com>
2022-03-04 14:09:49 +0300

    feat: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
       ConfigModule.forRoot(),
+      TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env["DATABASE_HOST"],
+          port: parseInt(process.env["DATABASE_PORT"]),
+          username: process.env["DATABASE_USERNAME"],
+          password: process.env["DATABASE_PASSWORD"],
+          database: process.env["DATABASE_NAME"],
+          entities: [ __dirname + "/entity/*.{js,ts}"],
+          synchronize: true,
+      })
   ],
   controllers: [AppController],
   providers: [AppService],
