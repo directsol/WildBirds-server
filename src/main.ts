b652f1f6e26b8202cec5207cbef101c044198a54
directsol <sergey.n.mamontov@gmail.com>
2022-03-04 13:47:11 +0300

    feat: add fastify middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env['PORT']);
+  const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter()
+  );
+  await app.listen(process.env['FASTIFY_PORT']);
 }
+
 bootstrap();
